d7c51222684b0b32538aa83b8b2de74a56bc5b9e
Douglas Freitas <douglasofreitas@gmail.com>
2019-08-05 08:22:29 -0300

    test

---
+++ test/resources/user/user.resolvers.test.ts
@@ -0,0 +1,56 @@
+import { app, chai, db, handleError, expect } from './../../test-utils';
+
+describe('User', () => {
+
+    beforeEach(() => {
+        return db.Comment.destroy({where: {}})
+            .then((rows: number) => db.Post.destroy({where: {}}))
+            .then((rows: number) => db.Post.destroy({where: {}}))
+            .then((rows: number) => {
+                return db.User.create({
+                    name: 'Peter',
+                    email: 'peter@mail.com',
+                    password: '1234'
+                })
+            })
+    })
+
+    describe('Queries', () => {
+
+        describe('application/json', () => {
+
+            describe('users', () => {
+
+                it('should return a list of Users', () => {
+                    
+                    let body = {
+                        query: `
+                            query {
+                                users {
+                                    name
+                                    email
+                                }
+                            }
+                        `
+                    };
+
+                    return chai.request(app)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .send(JSON.stringify(body))
+                        .then(res => {
+                            const usersList = res.body.data.users;
+                            expect(res.body.data).to.be.an('object');
+                            expect(usersList).to.be.an('array').of.length(1);
+                            expect(usersList[0]).to.not.have.keys(['id', 'photo']);
+                            expect(usersList[0]).to.have.keys(['name', 'email']);
+                        }).catch(handleError);
+                })
+
+            });
+
+        })
+
+    })
+
+})
